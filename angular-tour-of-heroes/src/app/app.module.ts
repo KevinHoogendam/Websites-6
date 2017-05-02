31f73193a5019aab1adef384c7cbcfeef9e2a7b3
Kevin Hoogendam <khoogendam6@gmail.com>
2017-05-02 18:21:36 +0200

    Refactor routes to a Routing Module

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,18 +4,48 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+import { RouterModule }   from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+    {
+      path: '',
+      redirectTo: '/dashboard',
+      pathMatch: 'full'
+    },
+    {
+      path: 'dashboard',
+      component: DashboardComponent
+    },
+    {
+      path: 'heroes',
+      component: HeroComponent
+    },
+    {
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    }
+  ])
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
