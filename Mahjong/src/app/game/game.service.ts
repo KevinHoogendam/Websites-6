f36d1bc7da3792297bdbf87f43738e2bd4a1fc89
Kevin Hoogendam <khoogendam6@gmail.com>
2017-05-17 16:49:53 +0200

    post method

--- Mahjong/src/app/game/game.service.ts
+++ Mahjong/src/app/game/game.service.ts
@@ -14,11 +14,24 @@ const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.ImFnLmJsb21Ac3R1ZGVudC5hdmFu
 export class GameService {
     constructor(private http: Http) { }
     
-
     getGames(): Observable<Game[]> {
          return this.http.get('http://mahjongmayhem.herokuapp.com/games?pageSize=10')
                          .map((res:Response) => res.json())
                          .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
 
+    createGame(username: String, template: String, min: Number, max: Number): Observable<Game> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        let x-username = username;
+        let x-token = token;
+        let body: {
+            templateName = template,
+            minPlayers = min,
+            maxPlayers = max
+        };
+         return this.http.post('http://mahjongmayhem.herokuapp.com/games', body, {x-username, x-token}, options)
+                         .map((res:Response) => res.json())
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
     }
 }
