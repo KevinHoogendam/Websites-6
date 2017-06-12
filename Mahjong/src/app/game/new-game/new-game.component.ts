da510e8f76e9d795b8a7fd3aa1395a6e141a177f
Alec <agblom@student.avans.nl>
2017-06-12 14:12:29 +0200

    asddf

--- Mahjong/src/app/game/new-game/new-game.component.ts
+++ Mahjong/src/app/game/new-game/new-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Game } from '../game';
 
@@ -13,18 +14,20 @@ import { EmitterService } from '../../emitter.service';
   providers: [GameService]
 })
 export class NewGameComponent {
+  templates = ['Shanghai', 'Dragon', 'Monkey',
+            'Rooster', 'Snake', 'Ram'];
   model = new Game('Shanghai', 2, 32);
-  templates = ['Dragon', 'Monkey',
-            'Rooster', 'Snake', 'Ram', 'Shanghai'];
   
   constructor(private gameService: GameService) { }
   
   createGame(): void
   {
-    console.log('test');
-    return;
-    //console.log("create");
-    //this.gameService.createGame(this.username, this.templateName, this.minPlayers, this.maxPlayers)
+    let game = {
+                  'templateName': this.model.gameTemplate.id,
+                  'minPlayers': this.model.minPlayers,
+                  'maxPlayers': this.model.maxPlayers
+              };
+    console.log(game);
+    //this.gameService.createGame(game);
   }
-  onSubmit() { this.createGame(); }
 }
